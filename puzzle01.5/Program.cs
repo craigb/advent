7cc65b9b7dd00406d1f89f1f55767c3b46b95422
Craig Boucher <crbouch@microsoft.com>
2017-12-10 11:05:34 -0800

    Puzzle 1.5

---
+++ puzzle01.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace puzzle01._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var characters = args[0];
+            var sum = 0;
+            var step = characters.Length >> 1;
+
+            for (int i = 0; i < characters.Length; ++i)
+            {
+                var current = characters[i];
+                var otherIndex = (i + step) % characters.Length;
+                var next = characters[otherIndex];
+
+                if (current == next)
+                {
+                    sum += current - '0';
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
